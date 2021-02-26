03667153e17b0e7e8e4d37776b2a410cd160c482
boskyle <boskyle.orendain@gmail.com>
2021-02-25 19:08:24 -0500

    added first component along with rotue

--- bos-developer-portfolio/src/App.js
+++ bos-developer-portfolio/src/App.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import './App.css';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import Profile from './components/Profile/Profile';
+
+
 
 
 function App() {
   return (
+    <Router>
     <div className="App">
-    
+      <Switch>
+      <Route path="/" exact component={Profile}/>
+      </Switch>
     </div>
+    </Router>
   );
 }
 
---
+++ bos-developer-portfolio/src/components/Profile/Profile.js
@@ -0,0 +1,14 @@
+import React,{useState,useEffect} from 'react';
+
+
+function Profile () {
+
+    return (<h2>Profile route</h2>);
+
+
+}
+
+export default Profile;
+
+
+
