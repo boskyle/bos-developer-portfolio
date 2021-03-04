2f4cc6c6c2682e67ce724516edf2ad3e03401db2
boskyle <boskyle.orendain@gmail.com>
2021-03-04 17:24:36 -0500

    made changes to profile tab

--- bos-developer-portfolio/src/components/Nav/Nav.js
+++ bos-developer-portfolio/src/components/Nav/Nav.js
@@ -10,9 +10,9 @@ const Nav = () => {
 
     <div className="computer">
      <h2>boskyle@boskyle-dev-system Bosinux 5.10.15-1-BOS-TERMINAL x86_64 20.2.1</h2>   
-     <NavLink to="/"><h3>Profile</h3></NavLink>
-     <NavLink to="/projects"><h3>Projects</h3></NavLink>
-     <NavLink to="/experience"><h3>Experience</h3></NavLink>
+     <NavLink to="/" style={{ textDecoration: 'none' }}><h3>Profile</h3></NavLink>
+     <NavLink to="/projects" style={{ textDecoration: 'none' }}><h3>Projects</h3></NavLink>
+     <NavLink to="/experience"style={{ textDecoration: 'none' }}><h3>Experience</h3></NavLink>
      <div className="computer-contacts">
     <h3>Contact me</h3>
     <span></span>
--- bos-developer-portfolio/src/components/Profile/Profile.js
+++ bos-developer-portfolio/src/components/Profile/Profile.js
@@ -7,9 +7,22 @@ const Profile = () => {
     return (
         <div className="mainContainerItem">
 
-        <h2>Profile</h2>
+        {/* <h2>Profile</h2> */}
+        <div className="stats mt-5">
+        <span>
+        <h3>Name:   Boswell Orendain</h3>
+        <h3>Role:   Junior Web Developer</h3>
+        <h3>Hobby:  Fitness</h3>
+        </span>
+
+        </div>
 
 
+        <div className="summary-goal">
+       <div className="summary mr-5"><h3>SUMMARY</h3></div>
+       <div className="goal ml-5"><h3>GOAL</h3></div>
+
+        </div>
 
 
 
