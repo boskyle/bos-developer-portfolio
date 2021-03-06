1183434bea48a3a30f29e286c1472a0244968ef8
boskyle <boskyle.orendain@gmail.com>
2021-03-06 18:38:28 -0500

    added instructions to project.js

--- bos-developer-portfolio/src/components/Project/Project.js
+++ bos-developer-portfolio/src/components/Project/Project.js
@@ -8,6 +8,10 @@ const Project = () => {
         <div className="mainContainerItem"><h2>projects.</h2>
         
         <div className="projects-container">
+            {/* Project Item components will live here, these are just dummies for now */}
+            {/* 
+                I should pass Title, Modal pop up => {Description,Tech Used (pos array),Images}
+            */}
         <div className="project-item">1</div>
         <div className="project-item">2</div>
         <div className="project-item">3</div>
