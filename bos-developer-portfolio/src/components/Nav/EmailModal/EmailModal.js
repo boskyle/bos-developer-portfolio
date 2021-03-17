79882e6fef85a014dc6980d618d0de0101b5f039
boskyle <boskyle.orendain@gmail.com>
2021-03-17 18:33:31 -0400

    email form changes

--- bos-developer-portfolio/src/components/Nav/EmailModal/EmailModal.js
+++ bos-developer-portfolio/src/components/Nav/EmailModal/EmailModal.js
@@ -19,18 +19,21 @@ const EmailModal = ({isOpen,setIsOpen}) => {
     <div className="form-wrapper">
     <h2 className="">send me an email.</h2>
     <small onClick={EmailModalExit}>exit</small>
+
     <form className="email-form w-100 h-100 p-3"novalidate>
-        <div className="form-group">
-        <label htmlFor="sender-email">Your Email</label>
-        <input className="form-control w-25"/>
+        <div className="form-item d-flex flex-column">
+            <div className="form-group">
+            <label htmlFor="sender-email">Your Email</label>
+            <input className="form-control" placeholder="johnsmith@gmail.com"/>
+            </div>
+            <div className="form-group">
+            <label htmlFor="sender-email">Subject</label>
+            <input className="form-control" placeholder="Inquiry.."/>
+            </div>
+            <button  className="btn w-50 mx-auto mt-5">Push</button>
         </div>
 
-        <div className="form-group">
-        <label htmlFor="sender-email">Subject</label>
-        <input className="form-control w-25" placeholder="Application Inquiry.."/>
-        </div>
-
-
+        <div className="form-item d-flex flex-column"><h4>message</h4></div>
 
     </form>
     </div>
