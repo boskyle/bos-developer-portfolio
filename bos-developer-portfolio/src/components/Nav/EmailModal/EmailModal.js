aea1057c18c37be7a384cc3cd3150d8f9090aaac
boskyle <boskyle.orendain@gmail.com>
2021-03-18 13:15:37 -0400

    done validation for email sender

--- bos-developer-portfolio/src/components/Nav/EmailModal/EmailModal.js
+++ bos-developer-portfolio/src/components/Nav/EmailModal/EmailModal.js
@@ -15,7 +15,7 @@ const EmailModal = ({isOpen,setIsOpen}) => {
     }
 
     const onSubmit = (e) => {
-       e.preventDefault();
+      console.log('submit');
     }
 
     
@@ -23,18 +23,33 @@ const EmailModal = ({isOpen,setIsOpen}) => {
   
 
 
-    console.log(isOpen);
+   
     return<Modal isOpen={isOpen} className="container-email-form" overlayClassName="lg-overlay" ariaHideApp={false} onRequestClose={EmailModalExit}> 
 
     <div className="form-wrapper">
     <h2 className="">send me an email.</h2>
     <small onClick={EmailModalExit}>exit</small>
 
-    <form className="email-form w-100 h-100 p-3"novalidate>
+    <form className="email-form w-100 h-100 p-3" onSubmit={handleSubmit(onSubmit)} noValidate>
         <div className="form-item d-flex flex-column">
             <div className="form-group">
             <label htmlFor="sender-email">Your Email</label>
-            <input className="form-control" placeholder="johnsmith@gmail.com"/>
+            <input className="form-control" placeholder="johnsmith@gmail.com"
+            name="email"
+            type="email"
+            aria-describedby="emailInput"
+            ref={register({
+                required: {
+                    value: true,
+                    message: "Email required."
+                },
+                pattern: {
+                    value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
+                    message: "Not a valid email address.",
+                }
+              })}
+            />
+            {errors.email && <span className="form-err">{errors.email.message}</span>}   
             </div>
             <div className="form-group">
             <label htmlFor="sender-email">Subject</label>
@@ -46,8 +61,19 @@ const EmailModal = ({isOpen,setIsOpen}) => {
 <div className="form-item d-flex flex-column"><h4>message <span>{charCounter}/200</span></h4>
         <textarea className="form-control w-100 mt-1 h-50" placeholder="Hello World!"
         onChange={e => setCount(e.target.value.length)}
+        name="message"
+        type="text"
+        ref={register({
+            required: {
+                value: true,
+                message: "Message required."
+            },
+          maxLength: 200
+        })}
         />
-          <button  className="mt-5" onClick={onSubmit}>Send</button>
+         {errors.message && <span className="form-err">{errors.message.message}</span>}
+            <span className="form-err">{errors.message?.type === "maxLength" && "Message too long.."}</span>
+          <button type="submit" className="mt-5">SEND</button>
         </div>
 
     </form>
